d1ff39b5cb95bc87aa4a6016f3f4505ae626260e
Ygon farwynd Quaro <yquaro@e2r1p3.21-school.ru>
2019-03-19 15:59:16 +0300

    hello

--- sort_by_ascii.c
+++ sort_by_ascii.c
@@ -1,10 +1,11 @@
 #include "header.h"
 
-char	**put_to_matrix(char *dir_name, char **matrix)
+char	**put_to_matrix(char *dir_name)
 {
 	DIR				*dir;
 	struct dirent	*entry;
 	int 			i;
+	char			**matrix;
 
 	i = 0;
 	dir = opendir(dir_name);
@@ -13,17 +14,22 @@ char	**put_to_matrix(char *dir_name, char **matrix)
 		perror("diropen");
 		exit(1);
 	}
-	matrix = (char**)malloc(sizeof(char *) * number_of_files(dir_name));
+	matrix = (char**)malloc(sizeof(char *) * (number_of_files(dir_name) + 1));
+	if (matrix == NULL)
+		return (NULL);
 	while ((entry = readdir(dir)) != NULL)
 	{
 		matrix[i] = ft_strdup(entry->d_name);
 		// printf("%s\n", matrix[i]);
 		i++;
 	}
+	matrix[i] = NULL;
+	// printf("%s\n", matrix[i]);
+	closedir(dir);
 	return (matrix);
 }
 
-void	sort_by_ascii(char *dir_name) // освобождение памяти
+void	sort_by_ascii(char *dir_name) // дописать освобождение памяти
 {
 	char	**matrix;
 	int		i;
@@ -33,35 +39,48 @@ void	sort_by_ascii(char *dir_name) // освобождение памяти
 	swap_counter = 0;
 	i = 0;
 	num = number_of_files(dir_name); // вызываем функцию подсчета (1)
-	matrix = put_to_matrix(dir_name, matrix); // и тут (1)
+	matrix = put_to_matrix(dir_name); // и тут (1)
 
-	printf("matrix before sort\n");
-	while(i != 10)
-	{
-		printf("%s\n", matrix[i]);
-		i++;
-	}
-	i = 0;
+	// printf("matrix before sort\n");
+	// while(i != 15)
+	// {
+	// 	printf("%s\n", matrix[i]);
+	// 	i++;
+	// }
+	// i = 0;
+
+	// printf("\n\n\n");
+	// printf("strswap = %d\n", ft_strcmp(matrix[5], matrix[6]));
 	
-	while (swap_counter != num)
+	while (swap_counter != num - 1)
 	{
 		if (matrix[i + 1] == NULL)
+		{
 			i = 0;
+			swap_counter = 0;
+		}
+		// printf("strswap = %d\n", ft_strcmp(matrix[i], matrix[i + 1]));
 		if (ft_strcmp(matrix[i], matrix[i + 1]) >= 0)
 		{
+			// printf("matrix[i] = %s\nmatrix[i + 1] = %s\n", matrix[i], matrix[i + 1]);
 			matrix = ft_strswap(matrix, i, i + 1);
-			i++;
+			
+			// printf("matrix[i] = %s\nmatrix[i + 1] = %s\n\n\n", matrix[i], matrix[i + 1]);
 		}
 		else
 			swap_counter++;
-	}
-
-	i = 0;
-	printf("\n\nmatrix after sort\n");
-	while(i != 10)
-	{
-		printf("%s\n", matrix[i]);
 		i++;
 	}
+
+	// matrix = ft_matrixfree(&matrix, num + 1);
+
+
+	// i = 0;
+	// printf("\n\nmatrix after sort\n");
+	// while(i != 15)
+	// {
+	// 	printf("%s\n", matrix[i]);
+	// 	i++;
+	// }
 
 }
