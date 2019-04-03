1898abf130d1807ff24d0822eb9a874180e42443
wconnell <wconnell@student.21-school.ru>
2019-04-03 21:44:21 +0300

    flags.c now contains valid flag check and writes flags to the structure

--- flags.c
+++ flags.c
@@ -6,32 +6,101 @@
 /*   By: wconnell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 18:33:38 by wconnell          #+#    #+#             */
-/*   Updated: 2019/04/03 19:03:05 by wconnell         ###   ########.fr       */
+/*   Updated: 2019/04/03 21:42:27 by wconnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int	is_it_flag(char *s)
+int			is_it_flag(char *s)
 {
 	if(s && s[0] == '-' && s[1])
 		return(1);
 	else
 		return(0);
 }
-int find_flags(char **av, int ac, t_flags **flags)
+int			valid_flag(char *s, char *v)
 {
 	int i;
-
+	int j;
+	int f;
+	
 	i = 1;
-	while (i <= ac)
+	if (s[1] == '-' && !s[2])
+		return (2);
+	while (s[i] != '\0')
 	{
-		if (is_it_flag(av[i]))
-			putflag(av[i]);
-		else
-
+		j = 0;
+		f = 0;
+		while (v[j] != '\0')
+		{
+			if (s[i] == v[j])
+				f++;
+			j++;
+		}
+		if (f == 0)
+			usage(s[i]);
 		i++;
 	}
+	return (1);
+}
+int			putflag(char *av, t_flags *flags)
+{
+	int i;
+	int f;
 
-			
+	i = 1;
+	f = valid_flag(av, "lRatr");
+	if (f == 2)
+		return (2);
+	if (f)
+	{
+		while (av[i] != '\0')
+		{
+			av[i] == 'l' ? flags->l = 1 : 0;
+			av[i] == 'R' ? flags->R = 1 : 0;
+			av[i] == 'a' ? flags->a = 1 : 0;
+			av[i] == 't' ? flags->t = 1 : 0;
+			av[i] == 'r' ? flags->r = 1 : 0;
+			i++;
+		}
+	}
+	return (1);
+}
+int			find_flags(char **av, int ac, t_flags *flags)
+{
+	int		i;
+	int		f;
+	int		m;
+
+	i = 1;
+	f = 0;
+	while (i <= ac)
+	{
+		if (is_it_flag(av[i]) && f == 0)
+			m = putflag(av[i], flags);
+		else
+			f++;
+		if (is_it_flag(av[i]) && f)
+			bust(av[i]);
+		m == 2 ? f++ : 0;
+		i++;
+	}
+	return (1);
+}
+int main(int argc, char **argv)
+{
+	t_flags		flags;
+
+	flags = (t_flags){0, 0, 0, 0, 0};
+
+	if(argc > 1)
+	{
+		find_flags(argv, argc, &flags);
+	}
+	printf("l = %d\n", flags.l);
+	printf("R = %d\n", flags.R);
+	printf("a = %d\n", flags.a);
+	printf("t = %d\n", flags.t);
+	printf("r = %d\n", flags.r);
 }
--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 15:20:38 by yquaro            #+#    #+#             */
-/*   Updated: 2019/04/03 19:09:14 by wconnell         ###   ########.fr       */
+/*   Updated: 2019/04/03 21:41:28 by wconnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,15 @@ typedef struct		s_type
 	int		type;
 }					t_type;
 
+typedef	struct			s_flags
+{
+	int					l; // shows lots of shit 
+	int					R; // recursive shit
+	int					a; // show ".file" files
+	int					t; // sort by timestamp
+	int					r; // sort in reverse order
+}						t_flags;
+
 void	bust(char *file_name);
 
 void	no_such_check(char **names);
@@ -37,14 +46,10 @@ void	conclusion(char *dir_name);
 int		number_of_files(char *dir_name);
 char	**sort_by_ascii(char *dir_name);
 void	print(void);
+int						find_flags(char **av, int ac, t_flags *flags);
+int						is_it_flag(char *s);
+int						putflag(char *av, t_flags *flags);
+void					usage(char c);
 
-typedef	struct			t_flags
-{
-	int					l; // shows lots of shit 
-	int					R; // recursive shit
-	int					a; // show ".file" files
-	int					t; // sort by timestamp
-	int					r; // sort in reverse order
-}
 
 #endif
