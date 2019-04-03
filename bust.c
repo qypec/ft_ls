df9730063ada62af9f530dbdda65e9e81e642330
wconnell <wconnell@student.21-school.ru>
2019-04-03 21:51:16 +0300

    bust have a different functionality now

--- bust.c
+++ bust.c
@@ -6,7 +6,7 @@
 /*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 18:50:11 by yquaro            #+#    #+#             */
-/*   Updated: 2019/04/03 19:00:17 by yquaro           ###   ########.fr       */
+/*   Updated: 2019/04/03 20:23:03 by wconnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,14 @@ void	bust(char *file_name)
 	ft_puterror(file_name);
 	ft_puterror(": No such file or directory");
 	write(1, "\n", 1);
+	exit(1);
+}
+
+void	usage(char c)
+{
+	ft_putstr_fd("ls: illegal option -- ", 2);
+	ft_putchar_fd(c, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putstr_fd("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]\n", 2);
+	exit(1);
 }
