f74a76f315bca41baa7f4f7d0913ea3e49e3457e
wconnell <wconnell@student.21-school.ru>
2019-04-10 21:25:44 +0300

    fixed matrix sort2

--- matrix_sort.c
+++ matrix_sort.c
@@ -6,19 +6,49 @@
 /*   By: wconnell <wconnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 19:49:17 by wconnell          #+#    #+#             */
-/*   Updated: 2019/04/10 16:49:59 by wconnell         ###   ########.fr       */
+/*   Updated: 2019/04/10 21:24:42 by wconnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+char		**sort_by_ascii(char **s)
+{
+	int		i;
+	int		j;
+	int		f;
+
+	i = 0;
+	j = 0;
+	f = 1;
+	while (f)
+	{
+		i = 0;
+		f = 0;
+		while(s[i] != NULL)
+		{
+			if (s[i + 1] != NULL)
+			{
+				if (ft_strcmp(s[i], s[i + 1]) > 0)
+				{
+					ft_strswap(s, i, i + 1);
+					f++;
+				}
+			}
+			i++;
+		}
+	}
+	return(s);
+}
+
 void		sort_by_time(t_file *head, char **s)
 {
-	int	i;
-	int	k;
-	int	f;
-	t_file *first;
-	t_file *second;
+	int		i;
+	int		k;
+	int		f;
+	t_file	*first;
+	t_file	*second;
+
 	f = 1;
 	while (f)
 	{   
@@ -50,25 +80,22 @@ char        **reverse_arr(char **s)
     j = 0;
     while (s[i] != NULL)
         i++;
-	printf("%d\n", i);
     if (!(new = (char**)malloc(sizeof(char *) * i)))
         return (NULL);
     new[i] = NULL;
     i--;
-	printf("%s", s[0]);
     while (i >= 0)
     {
         new[j] = ft_strdup(s[i]);
         j++;
         i--;
     }
-	printf("%d\n", i);
-	printf("%d\n", j);
-    // ft_matrixfree(&s);
+    ft_matrixfree(&s);
     return(new);
 }
 char        **matrix_sort(t_file *head, char **s, t_flags *flags)
 {
+	s = sort_by_ascii(s);
 	if (flags->t == 1)
 		sort_by_time(head, s);
     if (flags->r == 1)
