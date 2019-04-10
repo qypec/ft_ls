c859fb55145be34cfd0bda211444c16b0bc55b40
wconnell <wconnell@student.21-school.ru>
2019-04-10 20:51:44 +0300

    fixed matrix sort

--- matrix_sort.c
+++ matrix_sort.c
@@ -3,40 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   matrix_sort.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
+/*   By: wconnell <wconnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 19:49:17 by wconnell          #+#    #+#             */
-/*   Updated: 2019/04/10 12:21:08 by yquaro           ###   ########.fr       */
+/*   Updated: 2019/04/10 16:49:59 by wconnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void        sort_by_time(t_file *head, char **s)
+void		sort_by_time(t_file *head, char **s)
 {
-     int    i;
-     int    k;
-     int    f;
-     t_file *first;
-     t_file *second;
-
-     f = 1;
-     while (f)
-     {   
-        i = 0;
-        f = 0;
-        while (s[i] != NULL)
-        {
-            first = find_list(&head, s[i]);
-            second = find_list(&head, s[i + 1]);
-            if (first->modif < second->modif)
-            {
-                s = ft_strswap(s, i, i + 1);
-                f++;
-            }
-            i++;
-        }
-     }
+	int	i;
+	int	k;
+	int	f;
+	t_file *first;
+	t_file *second;
+	f = 1;
+	while (f)
+	{   
+		i = 0;
+		f = 0;
+		while (s[i] != NULL)
+		{
+			first = find_list(&head, s[i]);
+			if(s[i + 1] != NULL)
+			{
+				second = find_list(&head, s[i + 1]);
+				if (first->modif < second->modif)
+				{
+					s = ft_strswap(s, i, i + 1);
+					f++;
+				}
+			}
+			i++;
+		}
+	}
 }
 char        **reverse_arr(char **s)
 {
@@ -48,24 +50,28 @@ char        **reverse_arr(char **s)
     j = 0;
     while (s[i] != NULL)
         i++;
+	printf("%d\n", i);
     if (!(new = (char**)malloc(sizeof(char *) * i)))
         return (NULL);
     new[i] = NULL;
     i--;
+	printf("%s", s[0]);
     while (i >= 0)
     {
-        new[j] = s[i];
+        new[j] = ft_strdup(s[i]);
         j++;
         i--;
     }
-    ft_matrixfree(&s);
+	printf("%d\n", i);
+	printf("%d\n", j);
+    // ft_matrixfree(&s);
     return(new);
 }
 char        **matrix_sort(t_file *head, char **s, t_flags *flags)
 {
-        if (flags->t == 1)
-            sort_by_time(head, s);
-        // if (flags->r == 1)
-            // reverse_arr(s);
-            return(s);
+	if (flags->t == 1)
+		sort_by_time(head, s);
+    if (flags->r == 1)
+        s = reverse_arr(s);
+    return(s);
 }
